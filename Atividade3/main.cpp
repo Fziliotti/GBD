4fc79ad857706eacf29eed74b0e566e965b67d64
felipe-2705 <felipeaugusto.ferreiradecastro@gmail.com>
2021-05-13 07:41:47 -0300

    free lista inteiros

--- Atividade3/main.cpp
+++ Atividade3/main.cpp
@@ -7,10 +7,13 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <map>
 #include <fstream>
 
 using namespace std;
 
+typedef map<string,int>LS_MAP;
+typedef struct lista_int{int offset; struct lista_int* prox;}LISTA_INT;
 // remove pontuacao de uma palavra
 void removePontuacao (char *palavra) {
     int length = strlen(palavra);
@@ -36,179 +39,100 @@ void imprimeLinha(int offset,FILE *f) {
 class listaInvertida {
 public:
     typedef struct registro_listaInvertida{int offset;int prox;}LI_REGISTRO; //offset da palavra, prox é o offset do proximo registro da lista
-    typedef struct registro_listaSecundaria{char palavra[100];int quantidade;int lista_offset;struct registro_listaSecundaria* prox;}LS_REGISTRO; //lista_offset é o offset do primero registro da lista invertida,  prox é um ponteiro pra o proximo item da lista de chaves
+    LS_MAP ls_palavras;
     FILE* f;
-    LS_REGISTRO* primeiro_registro = NULL; // aponta para o a cabeça da lista de chaves secundarias
 
     // construtor
     listaInvertida(){ f = fopen("listainvertida.dat","wb+"); }
     // destrutor
-    ~listaInvertida(){destroi_listasecundaria();fclose(f);}
+    ~listaInvertida(){ls_palavras.clear();fclose(f);}
 
     // adiciona palavra na estrutura
     void adiciona(char *palavra, int offset){
-         if(primeiro_registro == NULL){
-                primeiro_registro = novo_LS_REGISTRO();
-                strcpy(primeiro_registro->palavra,palavra);
-                if(!insere_lista_Invertida(primeiro_registro,offset)){printf("ERRO AO INSERIR NA LISTA!! ENCERRANDO FUNÇAO..."); exit(1);}
+         string p(palavra);
+         LS_MAP::iterator it;
+         pair<LS_MAP::iterator,bool>prt;
+         it = ls_palavras.find(p);
+         if(it == ls_palavras.end()){
+                // palavra nao existe na lista
+                prt = ls_palavras.insert(pair<string,int>(p,-1)); // insere um novo elemento apontando para nada
+                if(!insere_lista_Invertida(prt.first,offset)){printf("ERRO AO INSERIR NA LISTA!! ENCERRANDO FUNÇAO..."); exit(1);}
                 return;
              }
-
-        // vamos iterar na lista secundaria para encontrar o registro correto
-        LS_REGISTRO* aux =  primeiro_registro;
-        LS_REGISTRO* ant =  NULL;
-        int cmp;
-        while(1){
-            cmp = strcmp(palavra,aux->palavra);
-            if(cmp==0){
-                //encontro a palavra
-                break;
-            }
-             //checa se é o fim da lista
-            if(aux->prox == NULL){
-                //fim da lista;
-                aux->prox = novo_LS_REGISTRO();
-                strcpy(aux->prox->palavra,palavra);
-                aux = aux->prox;
-                break;
-            }
-
-            if(cmp<0){
-                // se for menor e ainda nao encontrou significa que a palavra nao esta na lista
-                LS_REGISTRO* novo = novo_LS_REGISTRO();
-                strcpy(novo->palavra,palavra);
-                if(ant!=NULL){
-                    ant->prox = novo;
-                }else{
-                    primeiro_registro = novo;
-                }
-                novo->prox = aux;
-                aux = novo;
-                break;
-            }
-            ant = aux;
-            aux = aux->prox;
-
-
-        }
-        // o registro a inserir a palavra sempre sai em aux
-        if(!insere_lista_Invertida(aux,offset)){printf("ERRO AO INSERIR NA LISTA!! ENCERRANDO FUNÇAO..."); exit(1);} //insere o novo offset para a palavra
+        // palavra ja existe na lista
+        if(!insere_lista_Invertida(it,offset)){printf("ERRO AO INSERIR NA LISTA!! ENCERRANDO FUNÇAO..."); exit(1);} //insere o novo offset para a palavra
     }
     // realiza busca, retornando vetor de offsets que referenciam a palavra
-    int * busca(char *palavra, int *quantidade) {
-        LS_REGISTRO* chave = busca_registroPorPalavra(palavra);
-        if(chave == NULL){
+    int* busca(char *palavra, int *quantidade){
+        LS_MAP::iterator it;
+        string s(palavra);
+        it = ls_palavras.find(s);
+        if(it == ls_palavras.end()){
             printf("Palavra nao encontrada....");
+            *quantidade = 0;
             return NULL;
         }
+        LISTA_INT* topo;
+        LISTA_INT* li;
+        LISTA_INT* aux;
         LI_REGISTRO r;
-        fseek(f,chave->lista_offset,SEEK_SET);
+        int pos = it->second;
+        fseek(f,pos,SEEK_SET);
         fread(&r,sizeof(LI_REGISTRO),1,f);
         // substituir pelo resultado da busca na lista invertida
-        quantidade[0] = chave->quantidade;
-        int *offsets = new int[*quantidade];
-        for(int i=0;i<quantidade[0];i++){
-            offsets[i] = r.offset;
+        li = (LISTA_INT*)malloc(sizeof(LISTA_INT));
+        li->offset = r.offset;
+        li->prox = NULL;
+        topo = li;
+        *quantidade = 1;
+        while(r.prox!=-1){
             fseek(f,r.prox,SEEK_SET);
             fread(&r,sizeof(LI_REGISTRO),1,f);
+            aux = (LISTA_INT*)malloc(sizeof(LISTA_INT));
+            aux->prox = li;
+            aux->offset = r.offset;
+            li = aux;
+            *quantidade+=1;
         }
-
+        int *offsets = new int[*quantidade];
+        for(int i=0;i<(*quantidade);i++){
+            offsets[i] = li->offset;
+            li = li->prox;
+        }
+        aux = topo->prox;
+        free(topo);
+        //desaloca a lista de inteiros
+        while(aux != NULL){
+            li = aux;
+            aux = aux->prox;
+            free(li);
+        }
         return offsets;
     }
 private:
 
-     // desaloca  a lista de chaves secundarias
-    void destroi_listasecundaria(){
-        if(primeiro_registro==NULL)return;
-
-        LS_REGISTRO* aux;
-        while(1){
-               aux =  primeiro_registro->prox;
-               free(primeiro_registro);
-               if(aux->prox == NULL){
-                break;
-               }
-               primeiro_registro = aux->prox;
-        }
-        return;
-    }
-
-    //cria um novo Registro da lista secundaria
-    LS_REGISTRO* novo_LS_REGISTRO(){
-        LS_REGISTRO* registro;
-        registro = (LS_REGISTRO*)malloc(sizeof(LS_REGISTRO));
-        registro->lista_offset = -1;
-        registro->quantidade = 0;
-        registro->prox = NULL;
-        return registro;
-    }
-
     //insere na lista Invertida
-    int insere_lista_Invertida(LS_REGISTRO* registro, int offset){
-        if(registro == NULL){return 0;}
-
+    int insere_lista_Invertida(LS_MAP::iterator it, int offset){
+        if(it == ls_palavras.end()){return 0;}
+        int novo_offset;
 
         LI_REGISTRO r;
         r.offset = offset;
-        r.prox = -1;
-
-        int registro_atual = registro->lista_offset;
-        LI_REGISTRO atual;
-        LI_REGISTRO ant;
-        int ant_offset= -1;
-        while(1){
-            if(registro_atual == -1){break;} // fim da lista
-            fseek(f,registro_atual,SEEK_SET);
-            fread(&atual,sizeof(LI_REGISTRO),1,f);
-            if(offset<=atual.offset){
-                break;
-            }
-            ant = atual;
-            ant_offset = registro_atual; // registro atual agor a é o anterior
-            registro_atual = atual.prox; //proximo registro
-
-        }
-        r.prox = registro_atual; // o registro a ser inserio aponta para o registro atual na posiçao
-        fseek(f,0,SEEK_END); // seek pro final do arquivo
-        int r_offset = ftell(f);
-        fwrite(&r,sizeof(LI_REGISTRO),1,f);//escreve o registro a ser inserido no fim do arquivo
-        if(ant_offset!=-1){
-            ant.prox = r_offset;
-            fseek(f,ant_offset,SEEK_SET);
-            fwrite(&ant,sizeof(LI_REGISTRO),1,f); // escreve o registro anterior se necessário
-        }else{
-            registro->lista_offset = r_offset;// caso o registro inserio seja o primeiro da lista faz apontar para a cabeça que esta na lista de chave secundaria
-        }
-        registro->quantidade++;
+        r.prox = it->second; // o novo registro aponta para o topo da lista
+        //// insere no final do arquivo
+        fseek(f,0,SEEK_END);
+        novo_offset = ftell(f); // salva posicao onde novo registro foi escrito
+        fwrite(&r,sizeof(LI_REGISTRO),1,f); // escreve novo registro
+        (*it).second = novo_offset; // topo da lista agora é o novo registro;
         return 1;
 
-    }
-
-    LS_REGISTRO* busca_registroPorPalavra(char* palavra){
-            int cmp = strcmp(palavra,primeiro_registro->palavra);
-            if(cmp == 0)
-            {
-                //encontrou a palavra
-                return primeiro_registro;
-            }
-            LS_REGISTRO* aux =  primeiro_registro->prox;
-            while(aux!=NULL){
-                cmp = strcmp(palavra,aux->palavra);
-                if(cmp==0){
-                    return aux;
-                }else if(cmp<0){
-                    return NULL;
-                }
-                aux = aux->prox;
-            }
-            return NULL;
     }
 };
 
 // programa principal
 int main(int argc, char** argv) {
     // abrir arquivo
-    ifstream in("biblia2.txt");
+    ifstream in("biblia.txt");
     if (!in.is_open()){
         printf("\n\n Nao consegui abrir arquivo biblia.txt. Sinto muito.\n\n\n\n");
     }
@@ -227,7 +151,7 @@ int main(int argc, char** argv) {
             removePontuacao(palavra);
             // desconsiderar palavras que sao marcadores do arquivo
             if (!((palavra[0] == '#') || (palavra[0] == '[') || ((palavra[0] >= '0') && (palavra[0] <= '9')))) {
-                printf("%d %s\n", offset,palavra); fflush(stdout); // debug :-)
+                //printf("%d %s\n", offset,palavra); fflush(stdout); // debug :-)
                 lista.adiciona(palavra, offset);
                 contadorDePalavras++;
                 if (contadorDePalavras % 1000 == 0) { printf(".");  fflush(stdout); }
